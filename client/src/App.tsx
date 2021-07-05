9670bc60ce6dc7551a48c027b359bc17d2b524ed
Mehedi Hasan <mehedihasanmasud06@gmail.com>
2021-07-05 13:05:54 +0600

    add: antd & reach-router are added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
 import './App.css';
+import { Button } from 'antd';
+import { Link } from "@reach/router";
 
 const App: FC = () => {
   let test: string = "Hello World";
   return (
     <div className="App">
       {test}
+      <Button type="primary">Button</Button>
     </div>
   );
 }
