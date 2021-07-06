0e5dd1dff24153744a3ee8fb325407a8bb761c96
Mehedi Hasan <mehedihasanmasud06@gmail.com>
2021-07-06 19:44:00 +0600

    add: login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import { FC } from 'react';
 import './App.css';
-import RegistrationForm from './components/register/RegistrationForm';
 
 const App: FC = () => {
-  let test: string = "Hello World";
   return (
     <div className="App">
-      {test}
-      <RegistrationForm/>
     </div>
   );
 }
