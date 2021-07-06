bb01658bfc83b5491f208c5197e1d7485c650ff6
Mehedi Hasan <mehedihasanmasud06@gmail.com>
2021-07-06 22:53:38 +0600

    add: navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 const App: FC = () => {
+  // const [count, setCount] = useState<number>(0);
   return (
     <div className="App">
+      {/* <p>The Count is {count}</p>
+      <button onClick={()=> setCount(count+1)}>Counter</button> */}
+      <Navbar/>
     </div>
   );
 }
