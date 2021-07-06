3fdab73f32752d68ebe7eef45e86a9520a25d415
Mehedi Hasan <mehedihasanmasud06@gmail.com>
2021-07-06 19:39:51 +0600

    register form is added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import { FC } from 'react';
 import './App.css';
-import { Button } from 'antd';
-import { Link } from "@reach/router";
+import RegistrationForm from './components/register/RegistrationForm';
 
 const App: FC = () => {
   let test: string = "Hello World";
   return (
     <div className="App">
       {test}
-      <Button type="primary">Button</Button>
+      <RegistrationForm/>
     </div>
   );
 }
